fdacfd0d904e8323c534d1b1d6b41311248caa47
tns-develop <tns-develop@users.noreply.github.com>
2023-01-26 23:13:31 +0000

    deploy: e9deb7696cb95db3e0cc554836befdbb68ab5df0

---
+++ _next/static/JAeBDofzXvICISpDvqVBE/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-e0611a99b9cd83a4.js"],"/_error":["static/chunks/pages/_error-dfcfa5bb62767c20.js"],"/linkSample":["static/chunks/pages/linkSample-d9c0d365daea633f.js"],sortedPages:["/","/_app","/_error","/linkSample"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
