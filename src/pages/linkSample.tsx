136763d1b6dff756f7208c18b5c814808541f10e
nakagawa.takeru <tns.develop@gmail.com>
2023-01-29 08:44:05 +0900

    GitHub Copilot導入テスト

--- src/pages/linkSample.tsx
+++ src/pages/linkSample.tsx
@@ -3,6 +3,8 @@ var aaa = "aaavc";
 export default function Home() {
   return (
     <div>
+      {/* // 現在の日時を表示するサンプルページを返却する */}
+      <p>現在の日時は{new Date().toLocaleString()}です。</p>
       <p>This is the sample pagew</p>
     </div>
   );
