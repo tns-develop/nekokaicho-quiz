3879939d17a9e54afa2e02845779627d07ac6cb0
nakagawa.takeru <tns.develop@gmail.com>
2023-01-29 10:44:59 +0900

    デプロイテスト

--- src/pages/linkSample.tsx
+++ src/pages/linkSample.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div>
       {/* // 現在の日時を表示するサンプルページを返却する */}
       <p>現在の日時は{new Date().toLocaleString()}です。</p>
-      <p>This is the sample pagewなんで変わらへんねん</p>
+      <p>This is the sample pagewなんで変わらへんねん！</p>
     </div>
   );
 }
