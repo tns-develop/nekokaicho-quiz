08e287bfeb7fef3a0f144d889e7a98b5ff598b84
nakagawa.takeru <tns.develop@gmail.com>
2023-02-11 12:12:20 +0900

    タイトル画像追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,38 @@
-import * as React from 'react';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import Link from 'next/link'
-import Copyright from './Copyright';
+import * as React from "react";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Link from "@mui/material/Link";
+import Copyright from "./Copyright";
+import Image from "next/image";
+import titleImage from "../../resources/image/titleImage.png";
 
 export default function Home() {
-  return (
-    <Container maxWidth="lg">
+	return (
+		<Container maxWidth="lg">
       <Box
-        sx={{
-          my: 4,
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <Typography variant="h4" component="h1" gutterBottom>
-          MUI v5 + Next.js with TypeScript exampleですねん
-          なっちゃんが勉強頑張ってますねん(*´ω｀*)
-        </Typography>
-        <Link href="/linkSample" color="secondary">
-          Go to the about page
-        </Link>
-        <Copyright />
-      </Box>
-    </Container>
-  );
+				sx={{
+					my: 4,
+					display: "flex",
+					flexDirection: "column",
+					justifyContent: "center",
+					alignItems: "center",
+          height: "100vh",
+				}}
+			>
+        <Image
+          src={titleImage}
+          alt="titleImage"
+          style={{ width: '100vw', height: '100vh' }}
+        />
+				<Typography variant="h4" component="h1" gutterBottom>
+					ねこかいちょークイズ！
+				</Typography>
+				<Link href="/linkSample" color="secondary">
+					Go to the about page
+				</Link>
+				<Copyright />
+			</Box>
+		</Container>
+	);
 }
