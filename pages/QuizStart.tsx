787bb174897e6a598996ab43660b36c76cb14e8d
nakagawa.takeru <tns.develop@gmail.com>
2023-02-21 09:09:30 +0900

    前のコミットのエラー対応

--- pages/QuizStart.tsx
+++ pages/QuizStart.tsx
@@ -5,27 +5,29 @@ import getQuizInfo from "../src/components/quizInfo";
 // 文字を一文字ずつ表示する関数をasyncで実装
 const interval = 100;
 const typeText = async (text: string, setText: (text: string) => void) => {
-  let i = 0;
-  while (i < text.length) {
-    setText(text.slice(0, i));
-    i++;
-    await new Promise((resolve) => setTimeout(resolve, interval));
-  }
+	let i = 0;
+	while (i < text.length) {
+		setText(text.slice(0, i));
+		i++;
+		await new Promise((resolve) => setTimeout(resolve, interval));
+	}
 };
 
 // クイズ開始画面
 const QuizStart = () => {
-  
-  const [question, setQuestion] = React.useState("");
-  React.useEffect(async () => {
-    const subject = localStorage.getItem("subject");
-    const counter = Number(localStorage.getItem("counter"));
-    const quizInfo = getQuizInfo(subject, counter);
-    const index = 1;
-    
-    await typeText(quizInfo[index].question, setQuestion);
-  }, []);
-  return (
+	const [question, setQuestion] = React.useState("");
+	React.useEffect(() => {
+		const asyncFunc = async () => {
+			const subject = localStorage.getItem("subject");
+			const counter = Number(localStorage.getItem("counter"));
+			const quizInfo = getQuizInfo(subject, counter);
+			const index = 1;
+
+			await typeText(quizInfo[index].question, setQuestion);
+		};
+    asyncFunc();
+	}, []);
+	return (
 		<Container maxWidth="lg">
 			<Box
 				sx={{
@@ -37,12 +39,12 @@ const QuizStart = () => {
 					height: "100vh",
 				}}
 			>
-        <Typography variant="h4" component="h1" gutterBottom>
-          {question}
-        </Typography>
+				<Typography variant="h4" component="h1" gutterBottom>
+					{question}
+				</Typography>
 			</Box>
 		</Container>
-  );
+	);
 };
 
 // getStaticPropsでsubjectとcounterを受け取る
